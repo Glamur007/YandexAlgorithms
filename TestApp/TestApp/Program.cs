769de14c4e75f2b9e827757009e2199edf42f3ba
Yana <glamur007@yandex.ru>
2021-09-04 17:18:06 +0500

    2

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TestSum
 {
@@ -6,47 +7,63 @@ namespace TestSum
     {
         static void Main(string[] args)
         {
-            // драгоценности
-            string j = Console.ReadLine();
-            // камни
-            string s = Console.ReadLine();
+            //string[] array = Console.ReadLine().Split(' ');
+            //int a = int.Parse(array[0]);
+            //int b = int.Parse(array[1]);
+            //Console.WriteLine(a + b);
 
-            int result = 0;
-            foreach (var c in s)
+            int a;
+            int b;
+            int r = 0;
+
+            String line;
+            try
             {
-                if (j.IndexOf(c) >= 0)
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("input.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
                 {
-                    ++result;
+                    //write the line to console window
+                    //Console.WriteLine(line);
+                    string[] array = line.Split(' ');
+                    a = int.Parse(array[0]);
+                    b = int.Parse(array[1]);
+                    r = a + b;
+                    //Read the next line
+                    line = sr.ReadLine();
                 }
+                //close the file
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Write("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.Write("Executing finally block.");
             }
 
-            Console.WriteLine(result);
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("output.txt");
+                //Write a line of text
+                sw.WriteLine(r.ToString());
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Write("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.Write("Executing finally block.");
+            }
         }
-        /*static void Main(string[] args)
-        {
-            // драгоценности
-            string j = Console.ReadLine();
-            // камни
-            string s = Console.ReadLine();
-
-            int result = 0;
-            foreach (char ch in s)
-            {
-                if (j.IndexOf(ch) >= 0)
-                {
-                    ++result;
-                }
-            }
-
-            Console.WriteLine(result);
-        }*/
-
-        /*static void Main()
-        {
-            string[] array = Console.ReadLine().Split(' ');
-            int a = int.Parse(array[0]);
-            int b = int.Parse(array[1]); ;
-            Console.WriteLine(a + b);
-        }*/
     }
 }
