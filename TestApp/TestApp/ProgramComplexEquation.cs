2f11541e8db3528c9cf6b2e9be9ebe38d76a8847
Yana <glamur007@yandex.ru>
2021-09-05 10:55:37 +0500

    all

---
+++ TestApp/TestApp/ProgramComplexEquation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestSum
+{
+    class ProgramComplexEquation
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            var res = new List<int>();
+
+            for (int i = -1000000; i <= 1000000; i++)
+            {
+                if ((c * i + d) == 0)
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+                if ((a==c)&&(b==d))
+                {
+                    Console.WriteLine("INF");
+                    return;
+                }
+                if (((double) ((double)(a * i + b) / (double)(c * i + d))) == 0)
+                {
+                    res.Add(i);
+                }
+            }
+            if (res.Count > 0)
+            {
+                Console.WriteLine(String.Join(" ", res.ToArray()));
+            }
+            else
+            {
+                Console.WriteLine("NO");
+                return;
+             }
+            return;
+        }
+    }
+}
---
+++ TestApp/TestApp/ProgramSumWithFile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace TestSum
+{
+    class ProgramSumWithFile
+    {
+        static void Main(string[] args)
+        {
+            //string[] array = Console.ReadLine().Split(' ');
+            //int a = int.Parse(array[0]);
+            //int b = int.Parse(array[1]);
+            //Console.WriteLine(a + b);
+
+            int a;
+            int b;
+            int r = 0;
+
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("input.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    //Console.WriteLine(line);
+                    string[] array = line.Split(' ');
+                    a = int.Parse(array[0]);
+                    b = int.Parse(array[1]);
+                    r = a + b;
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Write("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.Write("Executing finally block.");
+            }
+
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("output.txt");
+                //Write a line of text
+                sw.WriteLine(r.ToString());
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Write("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.Write("Executing finally block.");
+            }
+        }
+    }
+}
